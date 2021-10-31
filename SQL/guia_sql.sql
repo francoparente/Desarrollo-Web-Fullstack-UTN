97e98e424f4aaad97c6b1e0695d92cac904ae91c
Franco Parente <fparente14@frba.utn.edu.ar>
2021-10-31 00:57:41 -0300

    organizo repo y agrego lo de sql

---
+++ SQL/guia_sql.sql
@@ -0,0 +1,11 @@
+-- 1. Listar los nombres de los usuarios.
+-- 2. Calcular el saldo máximo de los usuarios que tienen más de 30 años. Investigar sobre la utilización de la función MAX().
+-- 3. Listar nombre y teléfono de los usuarios con teléfono NOKIA, BLACKBERRY o SONY. Investigar sobre la utilización de la función IN().
+-- 4. Contar los usuarios sin saldo o inactivos.
+-- 5. Listar los números de teléfono con saldo menor o igual a 300.
+-- 6. Calcular la suma de los saldos de los usuarios de la compañía telefónica NEXTEL. Investigar sobre la utilización de la función SUM().
+-- 7. Contar el número de usuarios por compañía telefónica. Investigar sobre la utilización de la función COUNT() y GROUPBY().
+-- 8. Contar el número de usuarios por nivel.
+-- 9. Listar los usuarios con nivel 2.
+-- 10. Mostrar el email de los usuarios que usan gmail. Investigar sobre la utilización de la función LIKE().
+-- 11. Listar nombre y teléfono de los usuarios con teléfono LG, SAMSUNG o MOTOROLA.
---
+++ SQL/pruebas.sql
@@ -0,0 +1,5 @@
+select * from usuario
+
+set sql_safe_updates = 0; -- buscar qué es...
+
+
