d8d07d779812010d76303616906eb3a7ae694c70
Franco Parente <fparente14@frba.utn.edu.ar>
2021-11-03 21:14:21 -0300

    pruebas sql

--- SQL/pruebas.sql
+++ SQL/pruebas.sql
@@ -1,5 +1,12 @@
-use curso_utn_neoris_test;
-select * from ;
+USE tolkien;
 
-set sql_safe_updates = 0; -- buscar qué es...
+SELECT * FROM tolkien.human;
+
+DELETE FROM human WHERE id >4;
+
+SELECT name FROM tolkien.human;
+
+-- UPDATE human SET father = "Arathorn II" WHERE id_human = 1;
+
+SET sql_safe_updates = 0; -- buscar qué es...
 
