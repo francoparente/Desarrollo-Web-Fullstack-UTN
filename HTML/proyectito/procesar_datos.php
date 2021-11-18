60db45c3d0ec27ac146fd6188a64e2817a63b08e
Franco Parente <fparente14@frba.utn.edu.ar>
2021-11-18 20:45:53 -0300

    clase 8/11

---
+++ HTML/proyectito/procesar_datos.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['nombre'])) {
+    echo $_POST['nombre'];
+}
+
+echo "<br>";
+
+if(isset($_POST['email'])) {
+    echo $_POST['email'];
+}
--- HTML/proyectito/welcome.php
+++ HTML/proyectito/welcome.php
@@ -1,11 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario en clase</title>
+</head>
+<body>
+    
+    <form action="procesar_datos.php" method="post">
+        Name: <input type="text" name="nombre" placeholder="ej. Pepito Grillo"><br>
+        E-mail: <input type="email" name="email" placeholder="ej. pepito_grillo@mail.com"><br>
 
-if(isset($_POST['nombre'])) {
-    echo $_POST['nombre'];
-}
+        <button type="submit">Enviar</button>
+    </form>
 
-echo "<br>";
-
-if(isset($_POST['email'])) {
-    echo $_POST['email'];
-}
+</body>
+</html>
